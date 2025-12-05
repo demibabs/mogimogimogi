c9f029ea8300b3d23bb9a1656b6f55ba6ec4510e
demibabs <demibabs@gmail.com>
2025-12-05 16:51:21 -0600

    Test

--- bot/commands/misc/wait-a-minute.js
+++ bot/commands/misc/wait-a-minute.js
@@ -9,7 +9,7 @@ module.exports = {
 
 		for (let i = 1; i <= 12; i++) {
 			await new Promise(resolve => setTimeout(resolve, 5000));
-			// If the bot shuts down, this editReply should be blocked by the monkey-patch
+			// If the bot shuts down, this editReply *should* be blocked by the monkey-patch
 			await interaction.editReply(`Timer: ${i * 5} / 60 seconds elapsed...`);
 		}
 
